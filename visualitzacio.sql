8f72890e0a45e8a15c101f3325e2864e171f7b1e
rubegine <rubenrecolons2001@ginebro.cat>
2021-02-25 17:01:44 +0100

    commit 2

---
+++ visualitzacio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE VISUALITZACIONS (
+    id_peli smallint(5) UNSIGNED,
+    id_soci varchar(10),
+    tempVist time NOT NULL,
+    PRIMARY KEY (id_peli, id_soci),
+    FOREIGN KEY (id_peli) REFERENCES PELLICULES (id_peli),
+    FOREIGN KEY (id_soci) REFERENCES SOCIS (id_soci)
+);
+
+INSERT INTO VISUALITZACIONS VALUES (1,3333,"23:23:23"),(2,2222,"23:23:23"),(3,1111,"23:23:23");
